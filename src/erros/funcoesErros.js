76a52e9b803251d15d3229ed8516d968e8a264c0
Iridiana Campos <iridianac.amarante@gmail.com>
2025-03-28 11:11:41 -0300

    Primeira versão

---
+++ src/erros/funcoesErros.js
@@ -0,0 +1,8 @@
+export default function tratarErros(erro) {
+    if (erro.code === 'ENOENT') {
+        throw new Error('Arquivo não encontrado');
+    } else {
+        return 'Erro na aplicação';
+    }
+}
+
